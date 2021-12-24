d1d2558f6afc2e12426955db635b8dba18d483eb
Grandhigowtham <96564774+Grandhigowtham@users.noreply.github.com>
2021-12-23 22:15:23 -0800

    Add files via upload

---
+++ profit or loss.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int cp,sp;
+	printf("enter any two numbers:");
+	scanf("%d%d",&cp,&sp);
+	if(cp>sp)
+	{
+		printf("loss");
+		
+	}
+	else
+	{
+		printf("profit");
+		
+	}
+}
