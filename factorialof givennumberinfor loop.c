6053e31dcec9ff3a0989ba16f62c24397e6f7b52
Grandhigowtham <96564774+Grandhigowtham@users.noreply.github.com>
2022-01-07 17:36:12 +0530

    Add files via upload

---
+++ factorialof givennumberinfor loop.c
@@ -0,0 +1,12 @@
+//write a c program find out the factorial of a given number n where 1<=n<=13//
+#include<stdio.h>
+main()
+{
+	int i,n,product;
+	scanf("%d",&n);
+	for(i=1;i<=n ;i++)
+	{
+		product *=i;
+	}
+	printf("%d",product);
+}
